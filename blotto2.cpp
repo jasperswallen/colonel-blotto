a187f92c898f634dd972edbc5e81135fec48b557
Jasper Swallen <swallen@usc.edu>
2019-04-08 00:54:30 -0700

    create blotto2

--- blotto2.cpp
+++ blotto2.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 3) {
         cout << "Must include 2 files with player moves\n";
         return -1;
@@ -17,41 +17,44 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    int player1[100] = {0};
-    int player1Count[10] = {0};
-    int player1Score = 0;
-    int player2[100] = {0};
-    int player2Count[10] = {0};
-    int player2Score = 0;
+    int player1[10] = {0};
+    int player2[10] = {0};
+    int player1Score, player2Score = 0;
 
-    for (int i = 0; i < 100; i++) {
-        int temp;
-        player1File >> temp;
-        player1[i] = temp;
-        player1Count[temp]++;
-        player2File >> temp;
-        player2[i] = temp;
-        player2Count[temp]++;
+    for (int i = 0; i < 10; i++) {
+        player1File >> player1[i];
+        player2File >> player2[i];
+    }
+
+    int player1T, player2T = 0;
+    for (int i = 0; i < 10; i ++) {
+        player1T += player1[i];
+        player2T += player2[i];
+    }
+
+    if (player1T != 100 || player2T != 100) {
+        cout << "Must place down 100 soldiers\n";
+        return -1;
     }
 
     for (int i = 0; i < 10; i++) {
-        if (player2Count[i] > player1Count[i]) {
+        if (player2[i] > player1[i]) {
             cout << "Player 2 won " << i << "\n";
             player2Score += i;
-        } else if (player1Count[i] > player2Count[i]) {
+        } else if (player1[i] > player2[i]) {
             cout << "Player 1 won " << i << "\n";
             player1Score += i;
         } else {
             cout << "It was a tie! No points assigned\n";
         }
 
-        cout << "Score: Player 1: " << player1Score << "\tPlayer 2: " << player2Score << "\n";
+        cout << "Score: Player 1: " << player1Score
+             << "\tPlayer 2: " << player2Score << "\n";
 
         if (player2Score >= 20) {
             cout << "Player 2 won!\n";
             return 0;
-        }
-        else if (player1Score >= 20) {
+        } else if (player1Score >= 20) {
             cout << "Player 1 won!\n";
             return 0;
         }
