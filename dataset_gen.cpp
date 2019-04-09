37d88b92cf98de02dfe6eb30e7535da007275f96
Jasper Swallen <swallen@usc.edu>
2019-04-08 17:42:53 -0700

    much cleaner implementation of random dataset generation

--- dataset_gen.cpp
+++ dataset_gen.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     ofstream file("dataset.txt");
     if (file.fail() || argc != 2) {
         cout << "Unable to write\n";
@@ -16,23 +16,20 @@ int main(int argc, char* argv[]) {
 
     srand(time(NULL));
 
-    int a, b, c, d, e, f, g, h, i, j = 0;
-
     for (int x = 0; x < numOfIters; x++) {
-        a = rand() % 100;
-        b = rand() % (100 - a);
-        c = rand() % (100 - a - b);
-        d = rand() % (100 - a - b - c);
-        e = rand() % (100 - a - b - c - d);
-        f = rand() % (100 - a - b - c - d - e);
-        g = rand() % (100 - a - b - c - d - e - f);
-        h = rand() % (100 - a - b - c - d - e - f - g);
-        i = rand() % (100 - a - b - c - d - e - f - g - h);
-        j = 100 - a - b - c - d - e - f - g - h - i;
-        int sum = a+b+c+d+e+f+g+h+i+j;
-        file << i << " " << g << " " << e << " " << c << " " << a << " " << b << " " << d << " " << f << " " << h << " " << j << " " << sum << endl;
+        int numsoldiers[10] = {0};
+
+        for (int j = 0; j < 100; j++) {
+            numsoldiers[rand() % 10]++;
+        }
+        int sum = 0;
+        for (int j = 0; j < 10; j++) {
+            file << numsoldiers[j] << " ";
+            sum += numsoldiers[j];
+        }
+        file << sum << endl;
     }
-    
+
     cout << "Wrote to \"dataset.txt\"\n";
 
     return 0;
