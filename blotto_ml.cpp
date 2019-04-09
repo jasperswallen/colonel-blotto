fd92e4327ad8248de640de4bf4ddb208b65085c2
Jasper Swallen <swallen@usc.edu>
2019-04-09 15:39:52 -0700

    round robin! (basically machine learning at this point)

--- blotto_ml.cpp
+++ blotto_ml.cpp
@@ -11,88 +11,108 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    ifstream file(argv[1]);
+    ifstream player1File(argv[1]);
     ofstream oFile(argv[2]);
 
-    if (file.fail()) {
+    if (player1File.fail() || oFile.fail()) {
         cout << "Invalid files\n";
         return -1;
     }
 
-    int winner = 0;
+    int numOfWins = 0;
 
     int count;
-    file >> count;
+    player1File >> count;
     oFile << count << endl;
-    file.ignore();
-    int player1[10] = {0};
-    int player2[10] = {0};
-    int num = 0;
+    player1File.ignore();
 
-    while (!file.fail()) {
-        int player1Score = 0;
-        int player2Score = 0;
-        int player1T = 0;
-        int player2T = 0;
+    for (int num1 = 0; num1 < count; num1++) {
+        ifstream file(argv[1]);
+        file >> count;
+        file.ignore();
 
-        string player1S, player2S;
-        if (winner == 2 || num == 0 || winner == 0) {
-            getline(file, player1S);
-        }
-        if (winner == 1 || num == 0) {
-            getline(file, player2S);
-        }
+        int WL = 0;
 
+        string player1S;
+        getline(player1File, player1S);
         stringstream player1SS(player1S);
-        stringstream player2SS(player2S);
 
+        int player1[10] = {0};
+
+        int player1T = 0;
         for (int i = 0; i < 10; i++) {
-            if (winner == 2 || num == 0 || winner == 0)
-                player1SS >> player1[i];
-            if (winner == 1 || num == 0)
-                player2SS >> player2[i];
-
+            player1SS >> player1[i];
             player1T += player1[i];
-            player2T += player2[i];
         }
-
-        if (player1T != 100 || player2T != 100) {
-            cout << "Must place down 100 soldiers, invalid file\n";
+        if (player1T != 100) {
+            cout << "Player 1 does not add up to 100\n";
             return -1;
         }
 
-        winner = 0;
+        for (int num = 0; num < count; num++) {
+            int player2[10] = {0};
+            int player1Score = 0;
+            int player2Score = 0;
+            int player2T = 0;
 
-        for (int i = 0; i < 10; i++) {
-            if (player2[i] > player1[i]) {
-                player2Score += i;
-            } else if (player1[i] > player2[i]) {
-                player1Score += i;
+            string player2S;
+            getline(file, player2S);
+            stringstream player2SS(player2S);
+
+            for (int i = 0; i < 10; i++) {
+                player2SS >> player2[i];
+                player2T += player2[i];
             }
 
-            if (player2Score >= 20) {
-                for (int j = 0; j < 10; j++) {
-                    oFile << player2[j] << " ";
+            if (player2T != 100) {
+                cout << "Must place down 100 soldiers for P2, invalid file\n";
+                return -1;
+            }
+
+            for (int i = 0; i < 10; i++) {
+                if (player2[i] > player1[i]) {
+                    player2Score += (i + 1);
+                } else if (player1[i] > player2[i]) {
+                    player1Score += (i + 1);
                 }
-                oFile << 100 << endl;
-                winner = 2;
-                break;
-            } else if (player1Score >= 20) {
-                for (int j = 0; j < 10; j++) {
-                    oFile << player1[j] << " ";
+
+                if (player2Score >= 20) {
+                    WL--;
+                    break;
                 }
-                oFile << 100 << endl;
-                winner = 1;
-                break;
+                if (player1Score >= 20) {
+                    WL++;
+                    break;
+                }
             }
         }
-        if (!winner) {
-            if (player2Score > player1Score)
-                winner = 2;
 
-            if (player1Score > player2Score)
-                winner = 1;
+        file.close();
+
+        if (WL > 0) {
+            // that is, print out the line IFF it wins more than it loses
+            // against the test set (all posibilities)
+            for (int i = 0; i < 10; i++) {
+                oFile << player1[i] << " ";
+            }
+            oFile << 100 << endl;
+            numOfWins++;
         }
-        num++;
     }
+    player1File.close();
+    oFile.close();
+
+    ifstream newFile(argv[2]);
+    string *arr = new string[numOfWins + 1];
+    for (int i = 0; i <= numOfWins; i++) {
+        getline(newFile, arr[i]);
+    }
+    arr[0] = to_string(numOfWins);
+
+    ofstream overwrite(argv[2]);
+
+    for (int i = 0; i <= numOfWins; i++) {
+        overwrite << arr[i] << endl;
+    }
+    delete[] arr;
 }
